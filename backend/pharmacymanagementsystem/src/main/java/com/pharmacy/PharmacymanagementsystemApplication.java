9b37bb45e907d3e99f008ff0d02978d5d9654abc
Abuo Bernard <abuobernard@gmail.com>
2026-05-30 03:49:53 +0000

    vercel

--- backend/pharmacymanagementsystem/src/main/java/com/pharmacy/PharmacymanagementsystemApplication.java
+++ backend/pharmacymanagementsystem/src/main/java/com/pharmacy/PharmacymanagementsystemApplication.java
@@ -3,10 +3,15 @@ package com.pharmacy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class PharmacymanagementsystemApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
 		SpringApplication.run(PharmacymanagementsystemApplication.class, args);
 	}
 
